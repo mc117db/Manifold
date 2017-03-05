3f8247eb2547fca1933cafa146734275f7a6614d
Jon Jubelag <mc117db@gmail.com>
2017-03-05 21:27:05 +0800

    Hello World!

---
+++ Assets/Scripts/Transformation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Transformation : MonoBehaviour {
+
+    // Use this for initialization
+    public virtual Vector3 Apply(Vector3 point)
+    {
+        return Matrix.MultiplyPoint(point);
+    }
+    public abstract Matrix4x4 Matrix { get; }
+}
