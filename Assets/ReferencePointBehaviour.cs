077cc08683343d00e1ddacd3218ee00e2b965c52
Unknown <mc117db@gmail.com>
2017-03-07 19:07:42 +0800

    working

---
+++ Assets/ReferencePointBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReferencePointBehaviour : MonoBehaviour {
+	List<ReferencePointBehaviour> neighbours;
+	public bool Outer,Middle,Inner;
+	
+	public void AddNeighbour( ReferencePointBehaviour neighbour)
+	{
+		neighbours.Add(neighbour);
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/RingBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingBehaviour : MonoBehaviour {
+
+	RingData currentRingData;
+
+	public bool CombineRings (RingData other)
+	{
+		if (other.Outer == true && currentRingData.Outer == true)
+		{
+			return false;
+		}
+		else if (other.Outer != currentRingData.Outer)
+		{
+
+		}
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
