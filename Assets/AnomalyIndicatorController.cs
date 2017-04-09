863725d02570360f180ad0af388a5972d0e4e017
Unknown <mc117db@gmail.com>
2017-04-09 22:34:08 +0800

    Still working..

---
+++ Assets/AnomalyIndicatorController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnomalyIndicatorController : MonoBehaviour {
+
+	public bool active;
+	public ReferencePointBehaviour target;
+	public float lerpSpeed;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
